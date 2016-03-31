1c4dd1b0600623898414769d6870fe48947fef98
Tapash Adhikary <Tapash.Adhikary@gjensidige.no>
2016-03-31 17:56:55 +0530

    add angular 2

---
+++ angular-2-seed/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace angular_2_seed
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
